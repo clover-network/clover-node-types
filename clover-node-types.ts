1b86c8e22ffe1d0f5915e9b9fc02cfd4ee779364
wm-chen <18890318225@163.com>
2021-02-24 17:07:53 +0800

    ts

--- clover-node-types.ts
+++ clover-node-types.ts
@@ -1,14 +1,14 @@
-const cloverTypes = {
+export const cloverTypes = {
   Amount: 'i128',
   Keys: 'SessionKeys3',
   AmountOf: 'Amount',
   Balance: 'u128',
   CurrencyId: {
-    _enum: ['CLV', 'CUSDT', 'DOT', 'CETH']
+      _enum: ['CLV', 'CUSDT', 'DOT', 'CETH']
   },
   CurrencyIdOf: 'CurrencyId',
   CurrencyTypeEnum: {
-    _enum: ['CLV', 'CUSDT', 'DOT', 'CETH']
+      _enum: ['CLV', 'CUSDT', 'DOT', 'CETH']
   },
   PairKey: 'u64',
   Rate: 'FixedU128',
@@ -17,20 +17,18 @@ const cloverTypes = {
   Share: 'u128',
   OracleKey: 'CurrencyId',
   CurrencyInfo: {
-    id: 'CurrencyId',
-    name: 'CurrencyTypeEnum'
+      id: 'CurrencyId',
+      name: 'CurrencyTypeEnum'
   },
   ExchangeInfo: {
-    balance: 'Balance',
-    routes: 'Vec<CurrencyTypeEnum>'
+      balance: 'Balance',
+      routes: 'Vec<CurrencyTypeEnum>'
   },
   PoolId: {
-    _enum: {
-      Swap: 'u64'
-    }
+      _enum: {
+          Swap: 'u64'
+      }
   },
   EcdsaSignature: '[u8; 65]',
   EvmAddress: 'H160',
 }
-
-module.exports = cloverTypes;
